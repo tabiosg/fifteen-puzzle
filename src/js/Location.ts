4ab03f94f311674955658ebb5cc15b5f9055206c
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-12 13:54:50 -0400

    Update Location.ts

--- src/js/Location.ts
+++ src/js/Location.ts
@@ -2,12 +2,11 @@
 import { Direction } from './Direction.js';
 
 class Location {
-    // COMMENTS: these are the member variables of Rank
+    // COMMENTS: these are the member variables of Location.
     private row: number;
     private col: number;
 
-    // REQUIRES: 0 <= row <= 3 and c <= col <= 3
-    // EXAMPLES: row = 2, row = 3
+    // REQUIRES: 0 <= row <= 3 and c <= col <= 3.
     constructor(row: number, col: number) {
         assert(0 <= row && row < 4)
         assert(0 <= col && col < 4)
@@ -15,22 +14,22 @@ class Location {
         this.col = col;
     }
 
-    //EFFECTS: returns row of location
+    //EFFECTS: returns row of location.
     get getRow(): number {
         return this.row;
     }
 
-    //EFFECTS: returns col of location
+    //EFFECTS: returns col of location.
     get getCol(): number {
         return this.col;
     }
 
-    //EFFECTS: returns the index of the location in a grid
+    //EFFECTS: returns the index of the location in a grid.
     getIndex(): number {
         return this.row * 4 + this.col
     }
     
-    //REQUIRES: dir is a valid Direction and location in direction must be valid
+    //REQUIRES: dir is a valid Direction and location in direction must be valid.
     //EFFECTS: returns the location that is in the direction relative to the current location.
     getLocationInDirection(dir: Direction): Location {
         const rowDiffByLoc: Array<number> = [1, 0, -1, 0];
