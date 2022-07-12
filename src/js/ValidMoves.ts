18ff2b107cccc533f6bbb3a83083d41131b84891
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-12 14:52:55 -0400

    Update ValidMoves.ts

--- src/js/ValidMoves.ts
+++ src/js/ValidMoves.ts
@@ -3,27 +3,27 @@ import { Location } from './Location.js';
 
 class ValidMoves {
     // COMMENTS: these are the member variables of ValidMoves.
-    static valid_moves: Array<boolean>;
+    static validMoves: Array<Array<boolean>>;
 
     // REQUIRES: nothing.
     constructor() {
-        this.valid_moves = new Array<boolean>(16);
-        this.valid_moves = [];
+        ValidMoves.validMoves = new Array<Array<boolean>>(16);
+        ValidMoves.validMoves = [];
 
         for (let i: number = 0; i < 16; i++) {
-            this.valid_moves[i] = [true, true, true, true];
-            row: number = Math.floor(i/4);
-            col: number = i % 4;
-            if (row == 0) this.valid_moves[i][UP] = false;
-            else if (row == 3) this.valid_moves[i][DOWN] = false;
-            if (col == 0) this.valid_moves[i][LEFT] = false;
-            else if (row == 3) this.valid_moves[i][RIGHT] = false;
+            ValidMoves.validMoves[i] = [true, true, true, true];
+            const row: number = Math.floor(i/4);
+            const col: number = i % 4;
+            if (row == 0) ValidMoves.validMoves[i][Direction.Up] = false;
+            else if (row == 3) ValidMoves.validMoves[i][Direction.Down] = false;
+            if (col == 0) ValidMoves.validMoves[i][Direction.Left] = false;
+            else if (row == 3) ValidMoves.validMoves[i][Direction.Right] = false;
         }
     }
 
     //EFFECTS: returns whether it is valid to move an empty tile at a location in a certain direction
     static isMoveDirectionAtLocationValid(dir: Direction, loc: Location): boolean {
-        return valid_moves[loc.getIndex()][dir];
+        return ValidMoves.validMoves[loc.getIndex()][dir];
     }
 
 }
