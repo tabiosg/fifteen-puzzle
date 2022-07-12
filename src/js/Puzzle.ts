872ccff493a5da6fa77cfa3b5f44483090de7937
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-12 14:32:50 -0400

    Update Puzzle.ts

--- src/js/Puzzle.ts
+++ src/js/Puzzle.ts
@@ -21,35 +21,6 @@ class Puzzle {
         this.grid.swap(emptyLocation, movedTileLocation);
         Object.assign(this.emptyLocation, movedTileLocation);
     }
-    
-    //EFFECTS: sets grid equal to another grid.
-    setGrid(other: Grid): void {
-        this.tiles = Array.from(other.tiles);
-    }
-
-    //REQUIRES: loc is a valid location.
-    //EFFECTS: returns the tile number at the location.
-    getTileAtLocation(loc: Location): number {
-        return this.tiles[loc.getIndex()];
-    }
-    
-    //EFFECTS: resets the grid to have tiles at default locations.
-    reset(): void {
-        this.tiles = Array.from(default_tiles);
-    }
-    
-    //EFFECTS: shuffles the grid.
-    shuffle(): void {
-        // TODO - do eventually, don't just randomly shuffle unless you check if the grid is valid.
-    }
-    
-    //REQUIRES: locations are adjacent.
-    //EFFECTS: swaps the tiles of two adjacent locations.
-    swap(loc1: Location, loc2: Location): void {
-        index_1: number = loc1.getIndex();
-        index_2: number = loc2.getIndex();
-        [this.tiles[index_1] , this.tiles[index_2]] = [this.tiles[index_2] , this.tiles[index_1]];
-    }
 }
 
-export { Grid }
+export { Puzzle }
