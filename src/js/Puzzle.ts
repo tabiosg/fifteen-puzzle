1bb5e2ef03b8fbc16b4716207f3fe113ff4be814
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-13 13:15:28 -0400

    Update Puzzle.ts

--- src/js/Puzzle.ts
+++ src/js/Puzzle.ts
@@ -13,6 +13,41 @@ class Puzzle {
         // TODO - verify that this works
         this.grid = new Grid;
         this.emptyLocation = new Location(3, 3);
+        this.initializeAllButtons();
+    }
+
+    //EFFECTS: initialize buttons
+    initializeAllButtons(): void {
+        this.initializePuzzleMenuButtons();
+        this.initializeDirectionMenuButtons();
+    }
+
+    //EFFECTS: initialize puzzle menu buttons
+    initializePuzzleMenuButtons(): void {
+        let shuffleButton = document.getElementById("shuffle-button")!;
+        shuffleButton.onclick = () => {
+            this.grid.shuffle();
+        }
+
+        let resetButton = document.getElementById("reset-button")!;
+        resetButton.onclick = () => {
+            this.grid.reset();
+        }
+    }
+
+    //EFFECTS: initialize direction menu buttons
+    initializeDirectionMenuButtons(): void {
+        let directionMenuButtons = [
+            document.getElementById("up-button")!,
+            document.getElementById("right-button")!,
+            document.getElementById("down-button")!,
+            document.getElementById("left-button")!,
+        ]
+        for (let i = 0; i < directionMenuButtons.length; ++i) {
+            directionMenuButtons[i].onclick = () => {
+                this.movePuzzle(i);
+            }
+        }
     }
 
     //EFFECTS: moves grid in a given direction
